b1e52ed86876deb3345aee2d3795574f2f63fe86
Brian Pontarelli <brian@inversoft.com>
2014-01-14 17:19:52 -0700

    Fixing bugs and getting savant to be self building

--- src/main/shell/savant-maven-bridge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-working_dir=$(dirname $0)
-classpath=
-for f in $(ls $working_dir/../libs); do
-  classpath=${classpath}:${working_dir}/../libs/${f}
-done
-
-java -cp ${classpath} org.savantbuild.dep.maven.Main $@
